03baa5bb4214856248a2f2cc4b20e1a732ff84d9
viryl scott <viryl1515@gmail.com>
2022-04-23 13:13:05 +0100

    wip

---
+++ src/components/PageHome.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>Hello from home page</div>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>
+      {{ thread.title }}
+    </h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{ posts.find((p) => p.id === postId).text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from "@/data.json";
+export default {
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
